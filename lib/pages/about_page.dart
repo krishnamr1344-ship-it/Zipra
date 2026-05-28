3827b37fbd3eb4d2974fd774fd537a5776d57bd9
Mohanraj <mohanraj@Mohanrajs-MacBook-Pro.local>
2026-05-28 17:59:47 +0530

    Fix About page: show version dynamically via package_info_plus

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import '../constants/theme.dart';
 
-class AboutPage extends StatelessWidget {
+class AboutPage extends StatefulWidget {
   const AboutPage({super.key});
+
+  @override
+  State<AboutPage> createState() => _AboutPageState();
+}
+
+class _AboutPageState extends State<AboutPage> {
+  String _version = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _loadVersion();
+  }
+
+  Future<void> _loadVersion() async {
+    final info = await PackageInfo.fromPlatform();
+    if (mounted) setState(() => _version = '${info.version}+${info.buildNumber}');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +42,7 @@ class AboutPage extends StatelessWidget {
             const SizedBox(height: 16),
             const Text('Grocery App', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: AppColors.textPrimary)),
             const SizedBox(height: 4),
-            const Text('Version 1.0.0', style: TextStyle(fontSize: 14, color: AppColors.textSecondary)),
+            Text('Version $_version', style: const TextStyle(fontSize: 14, color: AppColors.textSecondary)),
           ],
         ),
       ),
