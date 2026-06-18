ebfd73d09d0c01e9e1406cceb046e4d6ba35e659
Mohanraj <mohanraj@Mohanrajs-MacBook-Pro.local>
2026-06-18 13:17:42 +0530

    chore: add debug logging to Cloudinary upload service

--- lib/services/cloudinary_service.dart
+++ lib/services/cloudinary_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import '../constants/cloudinary.dart';
 
@@ -10,15 +11,20 @@ class CloudinaryService {
     final request = http.MultipartRequest('POST', uri)
       ..fields['upload_preset'] = CloudinaryConfig.uploadPreset
       ..files.add(await http.MultipartFile.fromPath('file', filePath));
+    debugPrint('Cloudinary upload URL: $uri');
+    debugPrint('Cloudinary upload preset: ${CloudinaryConfig.uploadPreset}');
     final response = await request.send();
     final body = await response.stream.bytesToString();
+    debugPrint('Cloudinary response ($response.statusCode): $body');
     if (response.statusCode != 200) {
       final json = jsonDecode(body) as Map<String, dynamic>;
-      throw Exception(
-        (json['error'] as Map<String, dynamic>?)?['message'] ?? 'Upload failed',
-      );
+      final err = (json['error'] as Map<String, dynamic>?)?['message'] ?? 'Upload failed';
+      debugPrint('Cloudinary error: $err');
+      throw Exception(err);
     }
     final json = jsonDecode(body) as Map<String, dynamic>;
-    return json['secure_url'] as String;
+    final url = json['secure_url'] as String;
+    debugPrint('Cloudinary upload success: $url');
+    return url;
   }
 }
